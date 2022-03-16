6d229938f63c5c8edb00bba1880a240d3cb13bb1
AR123456 <coloradoz99@gmail.com>
2022-03-16 07:01:43 -0600

    implementing react-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h1>Mern Ecomm</h1>
+      <Header />
+      <main>
+        <h1>Mern Ecom</h1>
+      </main>
+      <Footer />
     </>
   );
 };
