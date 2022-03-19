34c45a3089316c7efbf8c680cf56338d373f00bc
AR123456 <coloradoz99@gmail.com>
2022-03-19 08:57:59 -0600

    front end rating component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,7 +8,7 @@ const HomeScreen = () => {
     <div>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={5} lg={4} xl={3}>
+          <Col key={product._id} sm={12} md={5} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
