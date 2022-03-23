bdd414d841d11c70756e9fb515a11b3c33cd9ab5
AR123456 <coloradoz99@gmail.com>
2022-03-23 06:44:56 -0600

    first get route

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,9 @@ const express = require("express");
 //  init express to app
 const app = express();
 const port = 5000;
+// setting up rotes
+app.get("/", (req, res) => {
+  res.send("API is running.... ");
+});
 //
-app.listen(5000, console.log(`Server is running on port ${port}`));
+app.listen(5000, console.log(`Server is running on http://localhost:${port}`));
