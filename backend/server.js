311ccab7cf2877a75eb943b4c756a5c0d29bfde0
AR123456 <coloradoz99@gmail.com>
2022-03-24 06:39:44 -0600

    frontend back end basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+// get dummy products from the package JSON file
+const products = require("./data/products");
 //  init express to app
 const app = express();
 const port = 5000;
-// setting up rotes
+// home route
 app.get("/", (req, res) => {
   res.send("API is running.... ");
 });
-//
+// products route - add this to the url and will see the products in JSON file
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+// get a single product by its ID -- add this to the url with a product id and will get that
+app.get("/api/products/:id", (req, res) => {
+  // using the same find method used on front end
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+// kick off server
 app.listen(5000, console.log(`Server is running on http://localhost:${port}`));
