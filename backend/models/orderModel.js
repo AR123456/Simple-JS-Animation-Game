2c4a6c644a1944213a1217f99a630c34d5c95aee
AR123456 <coloradoz99@gmail.com>
2022-04-01 06:42:54 -0600

    order data model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,91 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema(
+  {
+    // user associated with this order
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      // referance the user model
+      ref: "User",
+    },
+    // array of order items with stuff related to the order
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        // order has relationship to product
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress: {
+      // many imbedded objects
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    // scalable payment Methods
+
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    // this is data comming back from paypal
+    paymentResult: {
+      type: String,
+      status: String,
+      update_time: String,
+      // the email comming from pay pal
+      email_address: String,
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    // date and time paid at
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    // created at updated at fields can be created automatically this way
+    timestamps: true,
+  }
+);
+// create a model from this schema
+const Order = mongoose.model("Order", orderSchema);
+// export
+export default Order;
