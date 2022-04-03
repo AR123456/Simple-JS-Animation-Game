557d423eb72c30e0993bbe126bf0fd4945922dd2
AR123456 <coloradoz99@gmail.com>
2022-04-03 16:09:20 -0600

    fetch from DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-// this is just for fun for me to see colorful console.logs
 import colors from "colors";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+// import products from "./data/products.js";
+import productsRoutes from "./routes/productRoutes.js";
 
 // bring in dotenv
 dotenv.config();
@@ -18,16 +18,21 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running.... ");
 });
-// products route - add this to the url and will see the products in JSON file
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-// get a single product by its ID -- add this to the url with a product id and will get that
-app.get("/api/products/:id", (req, res) => {
-  // using the same find method used on front end
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+////// moving to productsRoute file
+// // products route - add this to the url and will see the products in JSON file
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+// // get a single product by its ID -- add this to the url with a product id and will get that
+// app.get("/api/products/:id", (req, res) => {
+//   // using the same find method used on front end
+//   const product = products.find((p) => p._id === req.params.id);
+//   res.json(product);
+// });
+/////////////// here mount router
+// anything going to /api/products will be linked to productRoutes
+app.use("/api/products", productRoutes);
+
 // get the port out of the .env file or use 5000
 const PORT = process.env.PORT || 5000;
 
