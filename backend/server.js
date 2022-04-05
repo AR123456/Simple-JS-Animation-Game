6ad5ad6c5ba548623a53ec0a4820a56da4acb2b0
AR123456 <coloradoz99@gmail.com>
2022-04-05 06:59:31 -0600

    custom err messg

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,29 @@ app.get("/", (req, res) => {
 // anything going to /api/products will be linked to productRoutes
 app.use("/api/products", productRoutes);
 
+// custom middleware
+// this one if for 400 errors
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+// with err middleware to override do err first then req, res, next
+app.use((err, req, res, next) => {
+  // may get 200 even if error, if it is a 200 then make it a 500 whichs
+  // is a server error, else send the status code
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  // set res.status to the status code
+  res.status(statusCode);
+  // respond with JSON object
+  res.json({
+    // get message from error objcect
+    message: err.message,
+    // stack trace if not in production
+    stack: process.env.NODE__ENV === "production" ? null : err.stack,
+  });
+});
+
 // get the port out of the .env file or use 5000
 const PORT = process.env.PORT || 5000;
 
