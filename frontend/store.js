9cc90c1cc35d66090c5cb468383b7d2fe5e54352
AR123456 <coloradoz99@gmail.com>
2022-04-05 07:00:28 -0600

    setting up the redux store

--- frontend/store.js
+++ frontend/store.js
@@ -3,7 +3,25 @@
 //combineReducers -each reducer will handle a pieces of funtionality (fetch from back end, product list, request part and success response )
 // applyMiddleware - like thunk is a middleware
 import { createStore, combineReducers, applyMiddleware } from "redux";
-// making reqeusts
+// making requests
 import thunk from "redux-thunk";
 // to be able to use the redux dev tools
 import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({});
+
+// put things to load initally
+const initialState = {};
+
+const middleware = [thunk];
+
+// only pass in one store per app but can use combineReducers
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware([...middleware]))
+);
+
+export default store;
+// this is implemented with a provider the provider comes from react redux
+// imported in index.js
