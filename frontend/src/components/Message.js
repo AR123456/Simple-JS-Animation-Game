3b455972d3e962bce31e646935a656373b512324
AR123456 <coloradoz99@gmail.com>
2022-04-18 05:43:23 -0600

    add to cart imports

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
-// children is the text to shoe
+// children is the text to show
 const Message = ({ variant, children }) => {
   return <Alert variant={variant}>{children}</Alert>;
 };
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,4 +1,26 @@
-import React from "react";
+// useEffect is where we want to dispatch from cart
+// as long as there is a product id in the URL we
+// want to add it to cart
+import React, { useEffect } from "react";
+// deal with redux state
+import { useDispatch, useSelector } from "react-redux";
+// need links from react router dom
+import { Link } from "react-router-dom";
+//bootstrap stuff
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+
+// handling alters and messages
+import Message from "../components/Message.js";
+// redux actions for this screen
+import { addToCart } from "../actions/cartActions.js";
 
 const CartScreen = () => {
   return (
