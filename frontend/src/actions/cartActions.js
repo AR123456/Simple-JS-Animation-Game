22081c31f3fe9a9ce415fa2e6b08804d2a657582
AR123456 <coloradoz99@gmail.com>
2022-04-18 05:42:42 -0600

    const reducer action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,39 @@
+// bring in axios to make request to API products id
+// to get the data for the product to add to cart
+import axios from "axios";
+// import the constants so we dont have to put them in parens
+// the constants are the reducer actions that get fired off
+// on the component
+import { CART_ADD_ITEM } from "../constants/cartConstants";
+
+// much like useEffect , redux thunk to pass function within function
+// need to pass in the id and quantity which we are getting out of the url
+// in addition to dispatch using getState so that we can add this to local storage
+// getState allows us to get entire state tree
+export const addToCart = (id, qty) => async (dispatch, getState) => {
+  // destructure data
+  const { data } = await axios.get(`/api/products/${id}`);
+  // pass what is being dispatched type:  payload:
+  // this s the stuff we want to display in the cart
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
+  // using getState(). then whatever we want.  Grab cart items
+  // after dispatch want to save what all the cart data that was dispatched
+  // to local storage
+  localStorage.setItem(
+    "cartItems",
+    // this returns a JS object so need to stringigy to add to localStorage
+    // to use this later will need to use JSON.parse to put it back into an object
+    JSON.stringify(getState().cart.cartItems)
+  );
+  // now have this in local storage so need to get into store in  store.js
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,14 +7,26 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
+// to make use of cartReducrs add to the store
+import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
-  // productList is the piece of state
+  // what is being put into state
+  // state name : what its called in reducer
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
-
-const initialState = {};
+// grabbing the cart items from local storage if  "cartItems" parse it and set if noting then set empty array
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+// putting cart items, token user that were put into
+//local state in the actions.js files into state
+const initialState = {
+  //cart
+  cart: { cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
