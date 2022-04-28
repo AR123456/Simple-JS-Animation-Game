03d7285da8802f0c813342cc7444823a89ca4d47
AR123456 <coloradoz99@gmail.com>
2022-04-28 06:10:11 -0600

    more user consts and reducers

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+} from "../constants/userConstants";
+export const login = (email, password) => async (dispatch) => {
+  // log in and get token
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
+    // send in header data with content type of application .json
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      // data we get back from "/api/users/login"
+      payload: data,
+    });
+    // set local storage - make it a string data here is the user object
+    //  _id: user._id,
+    // name: user.name,
+    // email: user.email,
+    //  isAdmin: user.isAdmin,
+    //  token: generateToken(user._id),
+    localStorage.setItem("userInfo", JSON.stringify(data));
+    // go back to store.js and load as initial value if its there
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,20 +8,13 @@ import {
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return {};
+      return { loading: true };
     case USER_LOGIN_SUCCESS:
-      return {};
+      return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
-      return {};
-    default:
-      return state;
-  }
-};
-export const userLogoutReducer = (state = {}, action) => {
-  switch (action.type) {
+      return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
-
     default:
       return state;
   }
