e147a97037f08e0ac5d6900485fbeceb2b7eaa7f
AR123456 <coloradoz99@gmail.com>
2022-05-13 06:52:33 -0600

    back end order by id route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,4 +39,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
-export { addOrderItems };
+
+//@desc get order by ID
+//@route GET/api/orders/:id
+//@access Private
+//TODO improve the security of this route by checking if admin or user sect58 Ch10 Q&A
+const getOrderById = asyncHandler(async (req, res) => {
+  // fetch the order - getting from URL - so params
+  // also need the name and user info of the user placing the order .populate()
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  // check to see if order exits if not throw error.
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found ");
+  }
+});
+export { addOrderItems, getOrderById };
