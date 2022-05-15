6639d79327b6ad7c45bc024babccdd3420b3c35a
AR123456 <coloradoz99@gmail.com>
2022-05-15 13:28:47 -0600

    db 500 error

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -81,8 +81,8 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       email_address: req.body.payer.email_address,
     };
     // const to save it in the DB
-    const updateOrder = await order.save();
-    res.json(updateOrder);
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
   } else {
     res.status(404);
     throw new Error("Order not found ");
