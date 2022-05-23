2c78a1d687930b9a771df89e4885e64eee86cd21
AR123456 <coloradoz99@gmail.com>
2022-05-23 06:23:41 -0600

    update user backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -136,6 +136,31 @@ const getUsersById = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 });
+// @desc    Admin Update user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  // get id from url
+  const user = await User.findById(req.params.id);
+  // check for user
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User p not found");
+  }
+});
 
 export {
   authUser,
@@ -145,4 +170,5 @@ export {
   getUsers,
   deleteUser,
   getUsersById,
+  updateUser,
 };
