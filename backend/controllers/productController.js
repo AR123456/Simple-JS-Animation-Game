c0b5f0388362d2a12dcaf38b8304eaf6c84d3c2b
AR123456 <coloradoz99@gmail.com>
2022-05-27 06:03:00 -0600

    back end create product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,6 +30,8 @@ const getProductById = asyncHandler(async (req, res) => {
 // @desc Delete product
 // @route DELETE/api/products/:id
 // @access Public/Admin
+//TODO this should actually be setting a flag isDeleted rather than deleting from the DB
+// deleting from DB messes up orders and any order history for user and likely seller tooooo
 const deleteProduct = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
   //TODO check to see if user is admin before allowing them to del products
@@ -46,5 +48,53 @@ const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found ");
   }
 });
+// when admin clicks create new product,create a sample with dummy data
+// then take admin to screen to edit that data
+// @desc Create product
+// @route POST/api/products
+// @access Public/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  // instantiate a new product
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  //var to hold new product , send to db
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+// @desc Update product
+// @route PUT/api/products/:id
+// @access Public/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  // from body get
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+  // Product model,id in URL
+  const product = await ProductlfindById(req.params.id);
+  // check for product
+  if (product) {
+    // set feilds
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
 
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
 export { getProducts, getProductById, deleteProduct };
