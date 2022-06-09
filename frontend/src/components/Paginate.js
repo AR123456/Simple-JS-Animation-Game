8f1a3833a7c3c3a2063c7810ba554742bcdac1b2
AR123456 <coloradoz99@gmail.com>
2022-06-09 07:12:23 -0600

    pagination but broken

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -14,11 +14,11 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1}
-            to={keyword ? `/search/${keyword}/page/${x + 1}` : `/page/${x + 1}`}
+            to={
+              keyword ? `/search/${keyword}/page/${x + 1}` : `/page/${(x = 1)}`
+            }
           >
-            <Pagination.Item active={x + 1 === page} activeLabel={false}>
-              {x + 1}
-            </Pagination.Item>
+            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
           </LinkContainer>
         ))}
       </Pagination>
