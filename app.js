c927f3ff69bd585f1036f09b1ed6392bec04f5d8
AR123456 <coloradoz99@gmail.com>
2022-08-11 05:49:59 -0600

    long way to endlessly scroll

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-// dynamic scroll speed
-let gameSpeed = 5;
+// dynamic scroll speed - in px
+let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "./backgroundLayers/layer-1.png";
@@ -15,10 +15,24 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = "./backgroundLayers/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "./backgroundLayers/layer-5.png";
-
+// horizontal position of background image
+let x = 0;
+// horizontal position of second duplicate background image
+let x2 = 2400;
 // animate background
 function animate() {
-  ctx.drawImage(backgroundLayer1, 0, 0);
+  CANVAS_HEIGHT;
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(backgroundLayer4, x, 0);
+  // detect end of image so that we can repeat, draw it twice so canvas is never empty  2400 is the width of the image
+  ctx.drawImage(backgroundLayer4, x2, 0);
+  // detect end of image so that we can repeat, draw it twice so canvas is never empty  2400 is the width of the image
+  if (x < -2400) x = 2400 + x2 - gameSpeed;
+  else x -= gameSpeed;
+  // detect end of image so that we can repeat, draw it twice so canvas is never empty  2400 is the width of the image
+  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+  else x2 -= gameSpeed;
   requestAnimationFrame(animate);
 }
 animate();
+// detect end of image so that we can repeat, draw it twice so canvas is never empty  2400 is the width of the image
