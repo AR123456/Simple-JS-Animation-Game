80e7fd8194c0055c6fffb6376c0a04f080238ecb
AR123456 <coloradoz99@gmail.com>
2022-08-12 06:48:15 -0600

    layers with game object

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
 // dynamic scroll speed - in px
-let gameSpeed = 5;
+let gameSpeed = 8;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = "./backgroundLayers/layer-1.png";
@@ -54,19 +54,23 @@ class Layer {
   }
 }
 // the word new looks for the class and triggers its constructor
-const layer1 = new Layer(backgroundLayer1, 0.5);
-const layer2 = new Layer(backgroundLayer2, 0.5);
-const layer3 = new Layer(backgroundLayer3, 0.5);
-const layer4 = new Layer(backgroundLayer4, 0.5);
+// pass each object a diffrent speed modifier
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
 const layer5 = new Layer(backgroundLayer5, 1);
+// game objects array with the layers
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 // animate background
 function animate() {
   CANVAS_HEIGHT;
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  layer4.update();
-  layer4.draw();
-  layer5.update();
-  layer5.draw();
+  // user for each to draw the layers  apply the call back function to update and draw
+  gameObjects.forEach((object) => {
+    object.update();
+    object.draw();
+  });
   requestAnimationFrame(animate);
 }
 animate();
