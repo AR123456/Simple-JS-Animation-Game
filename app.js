a94cab2228067ca5123d269090c76f45ab21472b
AR123456 <coloradoz99@gmail.com>
2022-08-15 06:22:44 -0600

    targeting first sprite

--- app.js
+++ app.js
@@ -6,33 +6,48 @@ CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
-// factory function to create ememys
+const enemyImage = new Image();
+enemyImage.src = "./enemies/enemy1.png";
 
+// factory function to create ememys
 class Enemy {
   constructor() {
     //randomizing starting positions
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
+    // size of one enemy in the row sheet is 1758/6
+    this.spriteWidth = 293;
+    this.spriteHeight = 133;
+    // this needs to be relative to sprite size to maintain aspect ratio
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
     // adding random speed between 2 and -2
     this.speed = Math.random() * 4 - 2;
   }
-  // methods avalable to all enemys
+
   update() {
-    // this.x++;
-    // this.y++;
     // random speed
     this.x += this.speed;
     this.y += this.speed;
   }
   draw() {
-    // this can be any drawing code, not just rectangles
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    // ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      // start at 0 0
+      0,
+      0,
+      // travel one sprite
+      this.spriteWidth,
+      this.spriteHeight,
+      // where to display the cropped out frame
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
-// call the class to generate an ememy
 
 // Create new enemies
 for (let i = 0; i < numberOfEnemies; i++) {
@@ -41,7 +56,7 @@ for (let i = 0; i < numberOfEnemies; i++) {
 // console.log(enemiesArray);
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  // methods from constructor
+
   enemiesArray.forEach((enemy) => {
     enemy.update();
     enemy.draw();
