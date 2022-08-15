89a7d648a89bd7d18695ee2b0be0d40a168bbd1c
AR123456 <coloradoz99@gmail.com>
2022-08-15 06:21:56 -0600

    random speed and movement using array methods

--- app.js
+++ app.js
@@ -3,56 +3,49 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 100;
+const enemiesArray = [];
 
 // factory function to create ememys
-// // starating with just one
-// enemy1 = {
-//   x: 10,
-//   y: 50,
-//   width: 200,
-//   height: 200,
-// };
 
 class Enemy {
   constructor() {
-    //starting coordinates
-    // this.x = 10;
-    // this.y = 50;
-    // this.width = 100;
-    // this.height = 100;
-    //randomizing starting positions of emenys
+    //randomizing starting positions
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
     this.width = 100;
     this.height = 100;
+    // adding random speed between 2 and -2
+    this.speed = Math.random() * 4 - 2;
   }
-  // methods avalable to all enemys created using this class. It will update the coordiates of the objects
+  // methods avalable to all enemys
   update() {
-    // for every frame increase x and y by one
-    this.x++;
-    this.y++;
+    // this.x++;
+    // this.y++;
+    // random speed
+    this.x += this.speed;
+    this.y += this.speed;
   }
   draw() {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 // call the class to generate an ememy
-const enemy1 = new Enemy();
-// creating an second
-const enemy2 = new Enemy();
-
+// const enemy1 = new Enemy();
+// Create new enemies
+for (let i = 0; i < numberOfEnemies; i++) {
+  // create new enemy and push to enemies array
+  enemiesArray.push(new Enemy());
+}
+console.log(enemiesArray);
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  // calling the update method defined in  in the constructor now
-  enemy1.update();
-  // enemy1.x++;
-  // enemy1.y++;
-  // enemy2.x += 0.5;
-  // enemy2.y += 0.5;
-  // now using the draw method defined in the class
-  // ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
-  enemy1.draw();
-
+  // methods from constructor
+  //get enemies from array and call the update and draw methond on each
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
   requestAnimationFrame(animate);
 }
 animate();
