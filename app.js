01157a1467835e43d93d161b3a4687607557bfe5
AR123456 <coloradoz99@gmail.com>
2022-08-17 06:51:55 -0600

    two to many enemies using sin wave move y axis

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 // increasing this gives more enemies
-const numberOfEnemies = 10;
+const numberOfEnemies = 20;
 const enemiesArray = [];
 // how fast the sheet is traversed
 let gameFrame = 0;
@@ -27,13 +27,28 @@ class Enemy {
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    // need this for sin wave
+    this.angle = 0;
+    // here randomizing this.angle
+    // this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
+    this.curve = Math.random() * 7;
   }
-  // movment - bats fly off page to the left never ending
+
   update() {
-    // this.x += Math.random() * 5 - 2.5;
-    // this bats fly off the page to the left
     this.x -= this.speed;
-    // this.y += Math.random() * 5 - 2.5;
+    // passing this.angle into Math.sin() to make wavy movement
+    // this.y += Math.sin(this.angle);
+    // to make more pronounced waves multiply bo 3 (cycle between -7 and 7)
+
+    // this.y += 7 * Math.sin(this.angle);
+    // instead of hard coding the 7 , could put in a var that is randomized so each enemy has diff movement
+    this.y += this.curve * Math.sin(this.angle);
+    // increase this.angle for every animation frame
+    // changing this value will change shape of sin wave
+    // this.angle += 0.1;
+    // can also randomize the angle change or shape of sin wave
+    this.angle += this.angleSpeed;
     // check for when the bats fly off page to the left then pull them back
     if (this.x + this.width < 0) this.x = canvas.width;
     // animate sprites
