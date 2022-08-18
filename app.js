9bcd4fba2f8d5a5983d535ba0015a65d0d3a46dd
AR123456 <coloradoz99@gmail.com>
2022-08-18 06:52:36 -0600

    set up

---
+++ app.js
@@ -0,0 +1,5 @@
+/**@type {HTML CanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 400;
+CANVAS_HEIGHT = canvas.height = 400;
