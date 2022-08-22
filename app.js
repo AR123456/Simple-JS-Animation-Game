4089b7adb34456cf030a4120aa501b45cb371bb6
AR123456 <coloradoz99@gmail.com>
2022-08-22 07:04:24 -0600

    starter code

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 /**@type {HTMLCanvasElement} */
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-CANVAS_WIDTH = canvas.width = window.innerWidth;
-CANVAS_HEIGHT = canvas.height = window.innerHeight;
-// holder for all raven objects
-let ravens = [];
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 500;
 
 let canvasPosition = canvas.getBoundingClientRect();
 
 // factory class -constructor update draw
-class Raven {
-  constructor() {
-    this.width = 100;
-    this.height = 50;
-    this.x = canvas.width;
-    // offset so that dont have 1/2 birds at top or bottom of screen
-    this.y = Math.random() * (canvas.height - this.canvas.height);
-    // horizontal speed 3 to 8
-    this.directionX = Math.random() * 5 + 3;
-    // vertical speed - 2.5 to + 2.5
-    this.directionY = Math.random() * 5 - 2.5;
-  }
-  // update - move raven and adjust any values that need to be before next frame drawn
-  update() {
-    // move to left
-    this.x -= this.directionX;
-  }
-  // draw - take updated values and any drawing code for a single raven object
-  draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-}
+
 // event listener
 
-// animation loop
-function animate(timeStamp) {
-  //frame by frame
-  // clear old paint
-  ctx.clearRect();
-}
+// animate
