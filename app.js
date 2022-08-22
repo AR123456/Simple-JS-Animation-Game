5a9a7a8c8ba3e9cd430455a55e8896ebc392e381
AR123456 <coloradoz99@gmail.com>
2022-08-22 07:03:25 -0600

    set up viewport for point and shoot game

---
+++ app.js
@@ -0,0 +1,13 @@
+/**@type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = window.innerWidth;
+CANVAS_HEIGHT = canvas.height = window.innerHeight;
+
+let canvasPosition = canvas.getBoundingClientRect();
+
+// factory class -constructor update draw
+
+// event listener
+
+// animate
