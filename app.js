6f8ab59edea0f4d6eefa05c749db8323ee80c118
AR123456 <coloradoz99@gmail.com>
2022-08-23 06:48:37 -0600

    delta array literal spread

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 /**@type {HTMLCanvasElement} */
+// 3 hrs in good explination
+////https://www.youtube.com/watch?v=GFO_txvwK_c
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = window.innerWidth;
@@ -27,9 +30,7 @@ class Raven {
     this.directionX = Math.random() * 5 + 3;
     // vertical speed - 2.5 to + 2.5
     this.directionY = Math.random() * 5 - 2.5;
-    // marked for deletion
-    // need to remove the ravens from the array after the travel off screen.  could use splice for this
-    // here using filter on the array to remove raves the are past the left edge of screen
+    // marked for deletion to prevent endlessly growing array
     this.markedForDeletion = false;
   }
   // update - move raven and adjust any values that need to be before next frame drawn
@@ -46,16 +47,13 @@ class Raven {
 }
 // use constructor to create raven object
 const raven = new Raven();
-// event listener
-
 // animation loop
 // the very first time the loop runs this timeStamp is undefined because it only gets created on second loop so need to give it a value on the first call of animate
 function animate(timeStamp) {
   //frame by frame
-  // create a new raven at interval that will work on old and new computers - time in miliseconds vs computers speed compare timeStamps to generate ravens
-
   // clear old paint
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // create a new raven at interval that will work on old and new computers - time in miliseconds vs computers speed compare timeStamps to generate ravens
   // delta time is the value in milliseconds between timestamp from this loop and saved timestamp value from previous loop
   let deltaTime = timeStamp - lastTime;
   lastTime = timeStamp;
@@ -66,14 +64,14 @@ function animate(timeStamp) {
   if (timeToNextRaven > ravenInterval) {
     // trigger raven class constructor to create one more raven pushed to the ravens array
     ravens.push(new Raven());
-    // set time to back to 0 to start count again
+    // set time back to 0 to start count again
     timeToNextRaven = 0;
     // console.log(ravens);
   }
   // cycle through the ravens array and call update and draw
   // this is an array literal- creates a new array on the fly
   // ... is the spread operator - spread the ravens array into this new array - expand into new array
-  // for each new object in the new array call update
+  // for each new object in the new array call update and draw
   // this array literal can be expanded for particles and enemies
   [...ravens].forEach((object) => object.update());
   [...ravens].forEach((object) => object.draw());
@@ -82,7 +80,7 @@ function animate(timeStamp) {
   //   object.draw();
   // });
   // need to remove the ravens from the array after the travel off screen.  could use splice for this
-  // here using filter on the array to remove raves the are past the left edge of screen
+  // here using filter on the array to remove raves the are past the left edge of screen double negative
   ravens = ravens.filter((object) => !object.markedForDeletion);
   // console.log(ravens);
   // call animate again to create endless loop
