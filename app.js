879c55e3ca9e1f5e00915217c0d12feff2667e09
AR123456 <coloradoz99@gmail.com>
2022-09-02 06:54:13 -0600

    removing extensive comments

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**@type {HTMLCanvasElement}*/
-// wait to run the js until all the html, images and CSS have been loaded.  This wraps everything
+
 // document.addEventListener("load", function () {
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -7,26 +7,24 @@ window.addEventListener("load", function () {
   canvas.width = 500;
   canvas.height = 800;
 
-  // Game class will be wrapper and contain all the movement and animation logic
+  // Game class will be wrapper movement and animation logic
   class Game {
     constructor() {
       this.enemies = [];
     }
-    // update and draw are public - need to be called from animation loop that is outside the constructor - they will handle the updating and drawing for the entire game - enemies player obstacles backgrounds menues
+    // update and draw are public handle the updating and drawing  entire game - enemies player obstacles backgrounds menus
     update() {}
     draw() {}
-    // private method will be called when need to create and set up a new enemy for game
-    // if methods name starts with # it will be a private class
-    // ic can only be called from within game class to manage some kind of internal functionality.  In this case adding a new enemy into the this.enemies array
+    // private method to create and set up a new enemy for game
     #addNewEnemy() {
       //need way to create new enemies for the game
     }
   }
-  // in this class declare blueprint - when called from the #addNewEnemy method it will create one new enemy ogject
+
   class Enemy {
-    // the ghosts worms and spiders will be crated by the enemy class but they will have different animations and different behaviors
+    // the ghosts worms and spiders animations and different behaviors
     constructor() {}
-    // update and draw methods in this class will handle updating and drawing each individual enemy position movement patterns sprite animation
+
     update() {}
     draw() {}
   }
@@ -35,13 +33,10 @@ window.addEventListener("load", function () {
   // animate has access to timestamp
   let lastTime = 1;
   function animate(timeStamp) {
-    // do this frame by frame
-    // start off by clearing the entire canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // some code -
-    // request AnimationFrame refreshes based on the machine so is scalable to all devices - only serve the next frame based on time between previous animate call and current animate call. Use built in timestamp. AKA delta time
+
     const deltaTime = timeStamp - lastTime;
-    // calculation done so reassign so ready to use for next calculation
+
     lastTime = timeStamp;
     console.log(deltaTime);
     console.log("hello");
