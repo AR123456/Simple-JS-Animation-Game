d66012f620421d2f21a816e9855d638dd1cef7bc
AR123456 <coloradoz99@gmail.com>
2022-09-07 07:27:26 -0600

    set up canvas and app js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
-  canvas.width = 500;
-  canvas.height = 800;
+  canvas.width = 800;
+  canvas.height = 720;
   class Game {
     constructor() {}
     update() {}
