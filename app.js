7854c73980bf2a4cbd06148792b406eeeabe6129
AR123456 <coloradoz99@gmail.com>
2022-09-08 07:20:35 -0600

    all arrow keydown and up

--- app.js
+++ app.js
@@ -14,21 +14,32 @@ window.addEventListener("load", function () {
       // when we created an instance of this class all code inside of it is executed. Note to take advantage of the lexical scoping of this so that the this.keys array is defined need to use an es6 arrow function for event listener
       //   window.addEventListener("keydown", function (e) {
       window.addEventListener("keydown", (e) => {
-        // don't allow duplicate "ArrowDown" entries.  if somethings index is -1 it means it is not present in the array
-        if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
+        // if somethings index is -1 it means it is not present in the array
+        if (
+          (e.key === "ArrowDown" ||
+            e.key === "ArrowUp" ||
+            e.key === "ArrowLeft" ||
+            e.key === "ArrowRight") &&
+          this.keys.indexOf(e.key) === -1
+        ) {
           // push to keys array
           this.keys.push(e.key);
         }
-        // console.log(e.key, this.keys);
+        console.log(e.key, this.keys);
       });
       window.addEventListener("keyup", (e) => {
         // don't allow duplicate "ArrowDown" entries.  if somethings index is -1 it means it is not present in the array
-        if (e.key === "ArrowDown") {
+        if (
+          e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight"
+        ) {
           // push to keys array
           //array.splice(index, howmany)
           this.keys.splice(this.keys.indexOf(e.keys), 1);
         }
-        // console.log(e.key, this.keys);
+        console.log(e.key, this.keys);
       });
     }
     update() {}
