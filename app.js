777084742a6ca27b2c4fc5d6c38613ea597bbf70
AR123456 <coloradoz99@gmail.com>
2022-09-08 07:20:00 -0600

    add with keyddown remove with key up

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 720;
-  // split responsiblitys between objects and how they interact
-  // Will handle event listeners, keyboard events, array of currently active keys
+  // split responsibilities between objects and how they interact
+  // event listeners, keyboard events, array of currently active keys
   class InputHandler {
     constructor() {
       // add and remove keys from this array as they are being pressed
@@ -14,24 +14,34 @@ window.addEventListener("load", function () {
       // when we created an instance of this class all code inside of it is executed. Note to take advantage of the lexical scoping of this so that the this.keys array is defined need to use an es6 arrow function for event listener
       //   window.addEventListener("keydown", function (e) {
       window.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowDown") {
+        // don't allow duplicate "ArrowDown" entries.  if somethings index is -1 it means it is not present in the array
+        if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1) {
           // push to keys array
           this.keys.push(e.key);
         }
-        console.log(e.key, this.keys);
+        // console.log(e.key, this.keys);
+      });
+      window.addEventListener("keyup", (e) => {
+        // don't allow duplicate "ArrowDown" entries.  if somethings index is -1 it means it is not present in the array
+        if (e.key === "ArrowDown") {
+          // push to keys array
+          //array.splice(index, howmany)
+          this.keys.splice(this.keys.indexOf(e.keys), 1);
+        }
+        // console.log(e.key, this.keys);
       });
     }
     update() {}
     draw() {}
     #addNewEnemy() {}
   }
-  // player class reacts to inputs or keys as they are being pressed drawing and updating the player
+  //reacts to inputs or keys as pressed, drawing and updating player
   class Player {
     constructor() {}
     update() {}
     draw() {}
   }
-  // handles endlessly scrolling background
+  // endlessly scrolling background
   class Background {
     constructor() {}
     update() {}
