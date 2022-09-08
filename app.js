93abcd21415a155385755cc34158c8af3d39e993
AR123456 <coloradoz99@gmail.com>
2022-09-08 07:19:06 -0600

    a note about es6 scope

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ window.addEventListener("load", function () {
     constructor() {
       // add and remove keys from this array as they are being pressed
       this.keys = [];
-      // when we created an instance of this class all code inside of it is executed
-      window.addEventListener("keydown", function (e) {
-        console.log(e);
+      // when we created an instance of this class all code inside of it is executed. Note to take advantage of the lexical scoping of this so that the this.keys array is defined need to use an es6 arrow function for event listener
+      //   window.addEventListener("keydown", function (e) {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === "ArrowDown") {
+          // push to keys array
+          this.keys.push(e.key);
+        }
+        console.log(e.key, this.keys);
       });
     }
     update() {}
