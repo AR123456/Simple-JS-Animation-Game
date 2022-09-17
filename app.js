2582fda8b43135dfca8c5a609e45d931de9cfd51
AR123456 <coloradoz99@gmail.com>
2022-09-17 05:27:55 -0600

    getting player on screen

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /**@type {HTMLCanvasElement} */
+// imports
+import Player from "./player.js";
 window.addEventListener("load", function () {
   // when game is loading show loader, when it is loaded stop showing loading
   const loading = this.document.getElementById("loading");
@@ -8,6 +10,12 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  // instansiate a player from the class
+  const player = new Player(canvas.width, canvas.height);
+  player.draw(ctx);
 
+  function animate() {
+    // bring in al the classes here to call their methods
+  }
   // end of window
 });
---
+++ player.js
@@ -0,0 +1,42 @@
+/**@type {HTMLCanvasElement} */
+
+// the player needs to be aware of the game boundrys so pass in as params
+export default class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    // array of possible player state
+    this.states = [];
+    // player can only have one state at a time, the index of the states array
+    this.currentState = this.states[0];
+    // point to the image in the html
+    this.image = document.getElementById("dogImage");
+    //1800x2182
+    // width by number of frames in the longest row 9/1800
+    this.width = 200;
+    // height of sprite sheet / the number of rows 2182/12
+    this.height = 181.83;
+    // postion from top left of canvas to start draw image put player on ground
+    this.x = this.gameWidth / 2 - this.width / 2;
+    this.y = this.gameHeight - this.height;
+    // horizontal navigation
+    this.frameX = 0;
+    // vertical navigation
+    this.frameY = 0;
+  }
+  //
+  draw(context) {
+    context.drawImage(
+      this.image,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  update() {}
+}
