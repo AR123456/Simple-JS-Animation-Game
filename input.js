32d4e1b31b98abd336e3c3b71528fa70b97e616e
AR123456 <coloradoz99@gmail.com>
2022-09-17 05:28:49 -0600

    input event handler

--- input.js
+++ input.js
@@ -1,6 +1,25 @@
 /**@type {HTMLCanvasElement} */
-export default class Input {
-  constructor() {}
+export default class InputHandler {
+  // keep track of keys
+  constructor() {
+    this.lastKey = "";
+    window.addEventListener("keydown", function (e) {
+      // expression to validate
+      switch (e.key) {
+        case "ArrowLeft":
+          // if arrowleft was pressed execute this code
+          this.lastKey = "PRESS left";
+      }
+    });
+    window.addEventListener("keyup", function (e) {
+      // expression to validate
+      switch (e.key) {
+        case "ArrowLeft":
+          // if arrowleft was pressed execute this code
+          this.lastKey = "RELEASE left";
+      }
+    });
+  }
   draw() {}
   update() {}
 }
