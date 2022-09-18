6105c61a54162c6d5abb5798301a302bb2f2e09f
AR123456 <coloradoz99@gmail.com>
2022-09-18 09:14:59 -0600

    passing standing l r classes

--- state.js
+++ state.js
@@ -12,45 +12,43 @@ class State {
   }
 }
 // extend class one of 4 pillars of object oriented programing
-class StandingLeft extends State {
+export class StandingLeft extends State {
   // if property cannot be found in child JS will look to the parent
   constructor(player) {
     // need to call super before this.
     super("STANDING LEFT");
     // make player class proptery
     this.player = player;
-}
-    // enter - everything the player needs to do when entering this state- like change speed or sprite sheet
-    enter(){
-        // this will run one time when we go into the state 
-        // animate row 1 
-        this.player.frameY=1;
-    }
-    // listen for a predefined set of inputs and swap to a different state when the correct key is pressed
-    handleInput(input){
-        // this will run over and over with each animation frame
-        // if we are standing left and press right set state to StandingRight 
-        if(input === "PRESS right")
-   
+  }
+  // enter - everything the player needs to do when entering this state- like change speed or sprite sheet
+  enter() {
+    // this will run one time when we go into the state
+    // animate row 1
+    this.player.frameY = 1;
+  }
+  // listen for a predefined set of inputs and swap to a different state when the correct key is pressed
+  handleInput(input) {
+    // this will run over and over with each animation frame
+    // if we are standing left and press right set state to StandingRight note that we could pass in 1 instead of states.STANDING_RIGHT but the words are more usnderstandable to humans
+    if (input === "PRESS right") this.player.setState(states.STANDING_RIGHT);
   }
 }
-class StandingRight extends State {
+export class StandingRight extends State {
   // if property cannot be found in child JS will look to the parent
   constructor(player) {
     // need to call super before this.
     super("STANDING RIGHT");
     // make player class proptery
     this.player = player;
-}
-    // enter - everything the player needs to do when entering this state- like change speed or sprite sheet
-    enter(){
-        // this will run one time when we go into the state 
-    }
-    // listen for a predefined set of inputs and swap to a different state when the correct key is pressed
-    handleInput(input){
-        // this will run over and over with each animation frame
-        // if we are in the standing right state and player pushes left set state to standing left
-        if(input==="PRESS left")
-    }
-
+  }
+  // enter - everything the player needs to do when entering this state- like change speed or sprite sheet
+  enter() {
+    // this will run one time when we go into the state
+  }
+  // listen for a predefined set of inputs and swap to a different state when the correct key is pressed
+  handleInput(input) {
+    // this will run over and over with each animation frame
+    // if we are in the standing right state and player pushes left set state to standing left. note that we could pass in 0 instead of states.
+    if (input === "PRESS left") this.player.setState(states.STANDING_LEFT);
+  }
 }
