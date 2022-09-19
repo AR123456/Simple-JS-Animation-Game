60c5371f37af9aebfa6f48bdff78a1856b8c1960
AR123456 <coloradoz99@gmail.com>
2022-09-19 06:19:01 -0600

    stand up one key release

--- state.js
+++ state.js
@@ -52,7 +52,9 @@ export class SittingLeft extends State {
   }
   handleInput(input) {
     if (input === "PRESS right") this.player.setState(states.SITTING_RIGHT);
-    else if (input === "PRESS up") this.player.setState(states.STANDING_LEFT);
+    // else if (input === "PRESS up") this.player.setState(states.STANDING_LEFT);
+    else if (input === "RELEASE down")
+      this.player.setState(states.STANDING_LEFT);
   }
 }
 export class SittingRight extends State {
@@ -65,6 +67,8 @@ export class SittingRight extends State {
   }
   handleInput(input) {
     if (input === "PRESS left") this.player.setState(states.SITTING_LEFT);
-    else if (input === "PRESS up") this.player.setState(states.STANDING_RIGHT);
+    // else if (input === "PRESS up") this.player.setState(states.STANDING_RIGHT);
+    else if (input === "RELEASE down")
+      this.player.setState(states.STANDING_RIGHT);
   }
 }
