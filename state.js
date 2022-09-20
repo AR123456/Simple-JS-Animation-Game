2221b152fafded4c0cd1a47707189c3abec47628
AR123456 <coloradoz99@gmail.com>
2022-09-20 06:31:21 -0600

    running left class in state js

--- state.js
+++ state.js
@@ -6,6 +6,8 @@ export const states = {
   STANDING_RIGHT: 1,
   SITTING_LEFT: 2,
   SITTING_RIGHT: 3,
+  RUNNING_LEFT: 4,
+  RUNNING_RIGHT: 5,
 };
 
 class State {
@@ -68,5 +70,19 @@ export class SittingRight extends State {
     if (input === "PRESS left") this.player.setState(states.SITTING_LEFT);
     else if (input === "RELEASE down")
       this.player.setState(states.STANDING_RIGHT);
+  }
+}
+export class RunningLeft extends State {
+  constructor(player) {
+    super("RUNNING LEFT");
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 7;
+  }
+  handleInput(input) {
+    if (input === "PRESS right") this.player.setState(states.RUNNING_RIGHT);
+    else if (input === "RELEASE left")
+      this.player.setState(states.STANDING_LEFT);
   }
 }
