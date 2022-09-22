ad793ad8add4e0443aca11e2d8c74c51eb8da701
AR123456 <coloradoz99@gmail.com>
2022-09-22 07:37:50 -0600

    clearing out for next project

--- utils.js
+++
@@ -1,9 +0,0 @@
-/**@type {HTMLCanvasElement} */
-// home of helper functions
-
-// function to display last key
-export function drawStatusText(context, input, player) {
-  context.font = "28px Helvetica";
-  context.fillText("Last input: " + input.lastKey, 10, 20);
-  context.fillText("Active state: " + player.currentState.state, 10, 70);
-}
