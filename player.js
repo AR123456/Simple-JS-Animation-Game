9575d7a120468c60207e933ef340aee949545548
AR123456 <coloradoz99@gmail.com>
2022-09-26 07:52:17 -0600

    stopping move off left side

--- player.js
+++ player.js
@@ -11,10 +11,18 @@ export class Player {
     this.x = 0;
     this.y = this.game.height - this.height;
     this.image = document.getElementById("player");
+    this.speed = 0;
+    this.maxSpeed = 10;
   }
   update(input) {
-    if (input.includes("ArrowRight")) this.x++;
-    else if (input.includes("ArrowLeft")) this.x--;
+    // horizontal
+    this.x += this.speed;
+    if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
+    else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
+    // keyup stops movement - no arrow key in the input array
+    else this.speed = 0;
+    // add boundries to player is not moving off screen
+    if (this.x < 0) this.x = 0;
   }
   draw(context) {
     // determines what the player looks like
