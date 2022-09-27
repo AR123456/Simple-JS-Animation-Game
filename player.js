5c76d9ec134beadafb8cb7ee7c748cac51605ad4
AR123456 <coloradoz99@gmail.com>
2022-09-27 07:27:27 -0600

    commented state pattern

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 /**@type {HTMLCanvasElement} */
 // all the player state imports
-
+import { Sitting } from "./playerStates.js";
 // Player class , constructor , draw  updates setting state
 export class Player {
   constructor(game) {
@@ -19,6 +19,12 @@ export class Player {
     this.image = document.getElementById("player");
     this.speed = 0;
     this.maxSpeed = 10;
+    // array of state vales - each state needs it own enter method and handle update method
+    this.states = [new Sitting()];
+    // points to indexes in the states array
+    this.currentState = this.states[0];
+    // when the player object is initialized for the first time call its enter methond to activate its initial defalt state
+    this.currentState.enter();
   }
   update(input) {
     // horizontal
