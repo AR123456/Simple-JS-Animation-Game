bd07c0ab74657917e12bbb1cb7f0a1cda6fd6ebe
AR123456 <coloradoz99@gmail.com>
2022-09-27 07:26:29 -0600

    boiler plate player state

--- playerStates.js
+++ playerStates.js
@@ -1,11 +1,12 @@
 /**@type {HTMLCanvasElement} */
+// enum of states object
+// each states enter and input handling
 
 // enums to make changing state more clear
 const states = {
   SITTING: 0,
   RUNNING: 1,
   JUMPING: 2,
-  FALLING: 3,
 };
 class State {
   constructor(state) {
@@ -15,63 +16,17 @@ class State {
   handleInput() {}
 }
 export class Sitting extends State {
-  constructor(player) {
-    super("SITTING");
-    this.player = player;
-  }
-  enter() {
-    this.player.frameY = 5;
-  }
-  handleInput(input) {
-    if (input.includes("ArrowLeft") || input.includes("ArrowRight")) {
-      this.player.setState(states.RUNNING);
-    }
-  }
+  constructor() {}
+  enter() {}
+  handleInput() {}
 }
 export class Running extends State {
-  constructor(player) {
-    super("RUNNING");
-    this.player = player;
-  }
-  enter() {
-    this.player.frameY = 3;
-  }
-  handleInput(input) {
-    if (input.includes("ArrowDown")) {
-      this.player.setState(states.SITTING);
-    } else if (input.includes("ArrowUp")) {
-      this.player.setState(states.JUMPING);
-    }
-  }
+  constructor() {}
+  enter() {}
+  handleInput() {}
 }
 export class Jumping extends State {
-  constructor(player) {
-    super("JUMPING");
-    this.state = player;
-  }
-  enter() {
-    if (this.player.onGround()) this.player.vy -= 30;
-    this.player.frameY = 1;
-  }
-  handleInput(input) {
-    // check for values on properties of player if vy is entering positive values and is more than the player weight player is falling
-    if (this.player.vy > this.player.weight) {
-      this.player.setState(states.FALLING);
-    }
-  }
-}
-export class Falling extends State {
-  constructor(player) {
-    super("FALLING");
-    this.state = player;
-  }
-  enter() {
-    this.player.frameY = 1;
-  }
-  handleInput(input) {
-    // wait for player to be on grouund
-    if (this.player.onGround) {
-      this.player.setState(states.RUNNING);
-    }
-  }
+  constructor() {}
+  enter() {}
+  handleInput() {}
 }
