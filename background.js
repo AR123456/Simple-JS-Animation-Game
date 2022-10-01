08307d70d894e47c4d19811ffdb7a9c20f20b4ce
AR123456 <coloradoz99@gmail.com>
2022-10-01 08:01:25 -0600

    why no background scroll

--- background.js
+++ background.js
@@ -16,7 +16,7 @@ class Layer {
   update() {
     // y is always 0
     // scroll the background sprite with no break when starting at the first frame again.
-    if (this.x < -this.width) this.x = 0;
+    if (this.x - this.width) this.x = 0;
     else this.x -= this.game.speed * this.speedModifier;
   }
   // draw takes in context to specify which gane element to draw on
@@ -38,10 +38,6 @@ export class Background {
     this.game = game;
     this.width = 1667;
     this.height = 500;
-    this.layer1image = document.getElementById("layer1");
-    this.layer2image = document.getElementById("layer2");
-    this.layer3image = document.getElementById("layer3");
-    this.layer4image = document.getElementById("layer4");
     this.layer5image = document.getElementById("layer5");
 
     // instance of Layer class
@@ -50,44 +46,10 @@ export class Background {
       this.width,
       this.height,
       1,
-      this.layer1image
-    );
-    this.layer2 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      1,
-      this.layer2image
-    );
-    this.layer3 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      1,
-      this.layer3image
-    );
-    this.layer4 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      1,
-      this.layer4image
-    );
-    this.layer5 = new Layer(
-      this.game,
-      this.width,
-      this.height,
-      1,
       this.layer5image
     );
     // array to hold background layers
-    this.backgroundLayers = [
-      this.layer1,
-      this.layer2,
-      this.layer3,
-      this.layer4,
-      this.layer5,
-    ];
+    this.backgroundLayers = [this.layer1];
   }
   update() {
     this.backgroundLayers.forEach((layer) => {
