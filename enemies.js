dbffe067d42af1660bb2cd522caff293d2ba9b8d
AR123456 <coloradoz99@gmail.com>
2022-10-02 05:42:18 -0600

    bringing in assets

---
+++ enemies.js
@@ -0,0 +1,16 @@
+/**@type {HTMLCanvasElement} */
+class Enemy {
+  constructor() {
+    this.frameX = 0;
+    this.frameY = 0;
+    this.fps = 20;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
+  }
+
+  update() {}
+  draw() {}
+}
+class FlyingEnemy extends Enemy {}
+class GroundEnemy extends Enemy {}
+class ClimbingEnemy extends Enemy {}
