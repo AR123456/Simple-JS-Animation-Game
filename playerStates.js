9aa06e01a4209cb12ca8df26090d46c6846f91ec
AR123456 <coloradoz99@gmail.com>
2022-10-06 06:16:11 -0600

    getting rolling state set up

--- playerStates.js
+++ playerStates.js
@@ -6,6 +6,9 @@ const states = {
   RUNNING: 1,
   JUMPING: 2,
   FALLING: 3,
+  ROLLING: 4,
+  DIVING: 5,
+  HIT: 6,
 };
 class State {
   constructor(state) {
@@ -46,6 +49,7 @@ export class Running extends State {
     } else if (input.includes("ArrowUp")) {
       this.player.setState(states.JUMPING, 1);
     }
+    // allow going into rolling from anywhere
   }
 }
 export class Jumping extends State {
@@ -78,6 +82,29 @@ export class Falling extends State {
   handleInput(input) {
     if (this.player.onGround()) {
       this.player.setState(states.RUNNING, 1);
+    }
+  }
+}
+export class Rolling extends State {
+  constructor(player) {
+    super("ROLLING");
+    this.player = player;
+  }
+  enter() {
+    this.frameX = 0;
+    this.player.maxFrame = 6;
+    // row 6 of sprite sheet
+    this.player.frameY = 6;
+  }
+  handleInput(input) {
+    // player should roll when enter key is held down
+    // if enter key not held down swith to running state
+    if (!input.includes("Enter") && this.player.onGround()) {
+      this.player.setState(states.RUNNING, 1);
+    }
+    // if player is in the air switch to falling
+    else if (!input.includes("Enter") && !this.player.onGround()) {
+      this.player.setState(states.FALLING, 1);
     }
   }
 }
