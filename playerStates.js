0c272aab601a5064edd7717491efe463d41f0fd1
AR123456 <coloradoz99@gmail.com>
2022-10-07 05:47:47 -0600

    refactor of player states

--- playerStates.js
+++ playerStates.js
@@ -1,5 +1,6 @@
 /**@type {HTMLCanvasElement} */
-
+// particles are closely tied to player state so putting here, but we need the game oject in each player state class too .  Need to do a bit of refactor for that.
+import { Dust } from "./particles.js";
 // enums to make changing state more clear
 const states = {
   SITTING: 0,
@@ -11,47 +12,51 @@ const states = {
   HIT: 6,
 };
 class State {
-  constructor(state) {
+  // passing n game here
+  constructor(state, game) {
     this.state = state;
+    // making game a class property
+    this.game = game;
   }
   enter() {}
   handleInput() {}
 }
+// as part of particle refactor need to replace all referances to player object with game. Game object has player on it so will still have access to player via game
 export class Sitting extends State {
-  constructor(player) {
-    super("SITTING");
-    this.player = player;
+  constructor(game) {
+    // now passing game in as second argument
+    super("SITTING", game);
   }
   enter() {
-    this.frameX = 0;
-    this.player.maxFrame = 4;
-    this.player.frameY = 5;
+    // now this needs to be this.game.player instead of this.player
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 4;
+    this.game.player.frameY = 5;
   }
   handleInput(input) {
     if (input.includes("ArrowLeft") || input.includes("ArrowRight")) {
-      this.player.setState(states.RUNNING, 1);
+      this.game.player.setState(states.RUNNING, 1);
     } else if (input.includes("Enter")) {
-      this.player.setState(states.ROLLING, 2);
+      this.game.player.setState(states.ROLLING, 2);
     }
   }
 }
 export class Running extends State {
-  constructor(player) {
-    super("RUNNING");
-    this.player = player;
+  constructor(game) {
+    super("RUNNING", game);
   }
   enter() {
-    this.frameX = 0;
-    this.player.maxFrame = 8;
-    this.player.frameY = 3;
+    this.game.player.frameX = 0;
+    this.game.player.maxFrame = 8;
+    this.game.player.frameY = 3;
   }
   handleInput(input) {
     if (input.includes("ArrowDown")) {
-      this.player.setState(states.SITTING, 0);
+      this.game.player.setState(states.SITTING, 0);
     } else if (input.includes("ArrowUp")) {
-      this.player.setState(states.JUMPING, 1);
+      this.game.player.setState(states.JUMPING, 1);
     } else if (input.includes("Enter")) {
-      this.player.setState(states.ROLLING, 2);
+      this.game.player.setState(states.ROLLING, 2);
     }
   }
 }
