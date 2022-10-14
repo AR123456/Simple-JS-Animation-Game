b6aae2f82c4e8de7aa2107b35e82758de8ab47bd
AR123456 <coloradoz99@gmail.com>
2022-10-14 04:54:09 -0600

    colision sprit row

--- collisionAnimation.js
+++ collisionAnimation.js
@@ -12,6 +12,9 @@ export class CollisionAnimation {
     this.frameX = 0;
     this.maxFrame = 4;
     this.markForDeletion = false;
+    this.fps = Math.random() * 10 + 5;
+    this.frameInterval = 1000 / this.fps;
+    this.frameTimer = 0;
   }
   draw(context) {
     context.drawImage(
@@ -26,8 +29,15 @@ export class CollisionAnimation {
       this.height
     );
   }
-  update() {
+  update(deltaTime) {
     // collision animation should move with game row
     this.x -= this.game.speed;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameX++;
+      this.frameTime = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+    if (this.frameX > this.maxFrame) this.markForDeletion = true;
   }
 }
