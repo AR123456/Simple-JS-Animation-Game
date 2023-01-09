5293f1065fe895e668a8633e896f38b88efc6ab7
AR123456 <coloradoz99@gmail.com>
2023-01-09 05:11:24 -0700

    start of refactor

--- particles.js
+++ particles.js
@@ -9,11 +9,11 @@ class Particle {
   update() {
     this.x -= this.speedX + this.game.speed;
     this.y -= this.speedY;
-    // adjust this to increase length of trail
-    this.size *= 0.97;
+    this.size *= 0.95;
     if (this.size < 0.5) this.markedForDeletion = true;
   }
 }
+// will need the exproted particesl in playerStates.js
 export class Dust extends Particle {
   constructor(game, x, y) {
     super(game);
@@ -22,7 +22,7 @@ export class Dust extends Particle {
     this.y = y;
     this.speedX = Math.random();
     this.speedY = Math.random();
-    this.color = "rgba(0,0,0,0.2)";
+    this.color = "black";
   }
   draw(context) {
     context.beginPath();
@@ -31,58 +31,5 @@ export class Dust extends Particle {
     context.fill();
   }
 }
-export class Splash extends Particle {
-  constructor(game, x, y) {
-    super(game);
-    this.size = Math.random() * 100 + 100;
-    this.x = x - this.size * 0.4;
-    this.y = y - this.size * 0.5;
-    this.speedX = Math.random() * 6 - 4;
-    this.speedY = Math.random() * 2 + 1;
-    this.gravity = 0;
-    this.image = document.getElementById("fire");
-  }
-  update() {
-    super.update();
-    this.gravity += 0.1;
-    this.y += this.gravity;
-  }
-  draw(context) {
-    context.drawImage(this.image, this.x, this.y, this.size, this.size);
-  }
-}
-export class Fire extends Particle {
-  constructor(game, x, y) {
-    super(game);
-    this.image = document.getElementById("fire");
-    // adjust the size of fire particles
-    this.size = Math.random() * 100 + 50;
-    this.x = x;
-    this.y = y;
-    this.speedX = 1;
-    this.speedY = 1;
-    // rotating the fire image
-    this.angle = 0;
-    // velocity of angle - change speed of rotation of fire image
-    this.va = Math.random() * 0.2 - 0.1;
-  }
-  update() {
-    super.update();
-    this.angle += this.va;
-    // horizontal wobble
-    this.x += Math.sin(this.angle * 5);
-  }
-  draw(context) {
-    context.save();
-    context.translate(this.x, this.y);
-    context.rotate(this.angle);
-    context.drawImage(
-      this.image,
-      -this.size * 0.5,
-      -this.size * 0.5,
-      this.size,
-      this.size
-    );
-    context.restore();
-  }
-}
+export class Splash extends Particle {}
+export class Fire extends Particle {}
